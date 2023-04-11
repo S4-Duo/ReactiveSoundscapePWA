41005fa01c8531d9f4dd9f86b0668e1153b597c4
brettmulderit <mulderit.brett@gmail.com>
2023-04-11 12:14:44 +0200

    Added more logic

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -3,7 +3,7 @@ import {SoundManager} from "@/Models/SoundManager";
 import {Sounds} from "@/Enums/Sounds";
 import {MotionManager} from "@/Models/MotionManager";
 
-export default function Homepage(){
+export default function Homepage() {
     const soundManager = new SoundManager()
     const motionManager = new MotionManager()
 
@@ -11,6 +11,13 @@ export default function Homepage(){
     const [gamma, setGamma] = useState<any>()
     const [alpha, setAlpha] = useState<any>()
     const [facing, setFacing] = useState<string>()
+
+    const [permission, setPermission] = useState<any>()
+
+    /**
+     * This function is being triggered when the user changed the motion of the devide
+     * @param event
+     */
     function handleMotionEvent(event: DeviceOrientationEvent) {
         setBeta(event.beta)
         setGamma(event.gamma)
@@ -18,23 +25,47 @@ export default function Homepage(){
         setFacing(motionManager.getDevicePosition(event.alpha!, event.beta!, event.gamma!))
     }
 
+    /**
+     * Ask for permission to the user, this needs to be here for some reason. I tried to add logic to the MotionManager
+     * But that one did not work. The user gets its prompt for allowing device motion. But than nothing happens. The
+     * useEffect with the event listener does not know if the user has given access or not
+     */
+    function askPermission() {
+        DeviceOrientationEvent.requestPermission().then(function (result) {
+            setPermission(result)
+            return console.log(result);
+        });
+        if (typeof DeviceOrientationEvent !== 'function') {
+            return console.log('DeviceOrientationEvent not detected')
+        }
+        if (typeof DeviceOrientationEvent.requestPermission !== 'function') {
+            return console.log('DeviceOrientationEvent.requestPermission not detected')
+        }
+    }
+
+    /**
+     * This useffect is used to play a sound when the user changed the facing to up or down
+     */
     useEffect(() => {
-        if (facing == "up"){
+        if (facing == "up") {
             console.log("value changed to up")
             soundManager.playSound("pickup")
-        }else if (facing == "down") {
+        } else if (facing == "down") {
             console.log("value changed to down")
             soundManager.playSound("putdown")
         }
     }, [facing])
 
+    /**
+     * This useffect is called for managing the event listener. When the deviceorientation changed, execute the handleMotionEvent
+     */
     useEffect(() => {
         if (typeof DeviceOrientationEvent.requestPermission === 'function') {
             DeviceOrientationEvent.requestPermission()
                 .then(permissionState => {
                     if (permissionState === 'granted') {
                         window.addEventListener("deviceorientation", handleMotionEvent);
-                    }else {
+                    } else {
                         window.addEventListener("deviceorientation", handleMotionEvent);
                     }
                 })
@@ -44,16 +75,20 @@ export default function Homepage(){
         }
     })
 
-
-
     return (
         <div>
             <h2>{beta}</h2>
             <h2>{gamma}</h2>
             <h2>{alpha}</h2>
             <h2>{facing}</h2>
-            <button onClick={motionManager.askPermission}>Grant Permission</button>
-            <button onClick={() => {soundManager.playSound("pickup")}}>Play Sound</button>
+            {
+                (permission != "") ? <button onClick={askPermission}>Ask for permission</button> : <></>
+            }
+
+            <button onClick={() => {
+                soundManager.playSound("pickup")
+            }}>Play Sound
+            </button>
         </div>
     );
 }
